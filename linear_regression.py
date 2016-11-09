70cfc9063ba07ead3bdb185fe14c740516d4251b
scelesticsiva <sivark@bu.edu>
2016-11-09 13:50:19 -0500

    Python code for linear regression and random forest
    Open source library sk-learn was used to build these model for the dataset we had and algorithms we used were linear regression and random forest

--- linear_regression.py
+++ linear_regression.py
@@ -7,22 +7,22 @@ import csv
 
 my_data=genfromtxt('training.csv',delimiter=',')
 print(my_data.shape)
-X=my_data[1:1001,1:2]
+X=my_data[1:1101,1:2]
 print(X.shape)
-for i in range(1000):
+for i in range(1100):
     for j in range(1):
         if(X[i][j]==np.NaN or X[i][j]==np.Infinity):
             X[i][j]=0.0
-y=my_data[1:1001,3599:3600]
-for k in range(1000):
+y=my_data[1:1101,3599:3600]
+for k in range(1100):
     for l in range(1):
         if(y[k][l]==np.NaN or y[k][l]==np.Infinity):
             y[k][l]=0.0
 print(y.shape)
 
-x_test=my_data[1002:1503,1:2]
-print(x_test.shape)
-y_test=my_data[1002:1503,3599:3600]
+X_test=my_data[52:71,1:2]
+print(X_test.shape)
+y_test=my_data[52:71,3599:3600]
 print(y_test.shape)
 
 """with open('training.csv','rb') as f:
@@ -43,18 +43,6 @@ sum=0
 
 plt.scatter(X,y,color='black')
 plt.plot(X,lr.predict(X),color='blue',linewidth=3)
-plt.xlabel('data')
-plt.ylabel('target')
-plt.title('Linear Regression')
 plt.xticks(())
 plt.yticks(())
 plt.show()
-'''subtract=y_test.flatten()-lr.predict(x_test)
-error=(((np.abs(subtract))**2).sum())/156
-print(error)'''
-sum_mean = 0
-for i in range(len(lr.predict(x_test))):
-    sum_mean += (lr.predict(x_test[i])-y_test[i])**2  
-    #sum_error=np.sqrt(sum_mean/predict_outcome.shape[0])
-print(sum_mean)
-print((sum_mean/lr.predict(x_test).shape[0])**0.5)
--- random_forest.py
+++ random_forest.py
@@ -6,33 +6,23 @@ import csv
 
 data_set=genfromtxt('training.csv',delimiter=',')
 print(data_set.shape)
-X=data_set[1:1001,1:2]
+X=data_set[1:1001,1200:1201]
 print(X.shape)
 x_test=data_set[1002:1503,1:2]
 print(x_test.shape)
 y=data_set[1:1001,3599:3600]
 y_test=data_set[1002:1503,3599:3600]
 y_flatten=np.ravel(y)
-rf=ensemble.RandomForestRegressor(1000)
+rf=ensemble.RandomForestRegressor(100)
 rf.fit(X,y)
 print((rf.predict(x_test)).shape)
 print(y_test.flatten().shape)
 #print(rf.score(x_test,rf.predict(x_test)))
 plt.scatter(X,y_flatten,color='black')
 plt.scatter(X,rf.predict(X),color='blue')
-
-'''subtract=y_test.flatten()-rf.predict(x_test)
+subtract=y_test.flatten()-rf.predict(x_test)
 error=(((np.abs(subtract))**2).sum())/156
-print(error)'''
-sum_mean = 0
-for i in range(len(rf.predict(x_test))):
-    sum_mean += (rf.predict(x_test[i])-y_test[i])**2  
-    #sum_error=np.sqrt(sum_mean/predict_outcome.shape[0])
-print(sum_mean)
-print((sum_mean/rf.predict(x_test).shape[0])**0.5)
+print(error)
 #plt.xticks(())
 #plt.yticks(())
-plt.xlabel('data')
-plt.ylabel('target')
-plt.title('Random Forest Regression')
 plt.show()
